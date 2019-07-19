e1654b4071bcaae79a97e1981e8b43b8d87d23c2
Kazuki <guruguru9606@gmail.com>
2019-07-19 22:10:58 +0900

    update

--- ocr.py
+++ ocr.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import pytesseract
 from PIL import Image
 import numpy as np
 
-img = Image.open('table.png')
+argv = sys.argv
+if len(argv) != 2:
+    print('Usage: ./ocr.py <filename>')
+
+img = Image.open(argv[1])
 im_list = np.array(img)
 
 txt = pytesseract.image_to_string(img)
